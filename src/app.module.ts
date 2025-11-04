b842b629f1fa0018bda48111aac01eb6b1c02e4e
SITP_USER1 <davevalencia17@gmail.com>
2025-11-04 16:17:47 +0900

    feat (common) - create paginated response and request

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TestimonyModule } from './testimony/testimony.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     PrismaModule,
     AuthModule,
+    TestimonyModule,
   ],
   controllers: [],
 })
---
+++ src/common/dto/paginated-request.dto.ts
@@ -0,0 +1,24 @@
+import { IsEnum } from 'class-validator/types/decorator/decorators';
+import { SortBy } from '../enum/sort-by.enum';
+
+export class PaginatedResponseDto<T> {
+  items: T;
+  pageIndex: number;
+  pageSize: number;
+  totalPages: number;
+  hasNext: boolean;
+  hasPrev: boolean;
+}
+
+export class PaginatedRequestDto {
+  searchTerm?: string;
+
+  pageIndex: number;
+
+  pageSize: number;
+
+  @IsEnum(SortBy)
+  sortBy: SortBy;
+
+  orderBy?: string;
+}
