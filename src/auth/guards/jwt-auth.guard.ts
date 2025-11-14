9cac37ddcc4ee574c3a413a23870375422f044ff
SITP_USER1 <davevalencia17@gmail.com>
2025-11-14 17:22:10 +0900

    configure (Jwt Auth Guard) - provide a different message when access token is expired

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,8 +1,13 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { STATUS_CODES } from 'http';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -22,5 +27,32 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     }
 
     return super.canActivate(context);
+  }
+
+  handleRequest(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ) {
+    if (err) {
+      throw err;
+    }
+
+    // if authentication failed
+    if (!user) {
+      // check info object
+
+      let message = 'Unauthorized';
+
+      if (info?.name == 'TokenExpiredError') {
+        // Token is expired
+        message = 'Token has expired';
+      }
+
+      // throw a customized exception
+      throw new UnauthorizedException(message);
+    }
   }
 }
